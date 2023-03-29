c72cb4c9aa634e84cf5e0cd7ee1910a0e741ce74
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2023-03-29 23:48:05 +0200

    fvfdvdf

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,23 @@
-# Install Nginx web server (w/ Puppet)
+# Install nginx with puppet
 package { 'nginx':
-  ensure => installed,
+  ensure   => '1.18.0',
+  provider => 'apt',
 }
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+file { 'Hello World':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World',
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+file_line { 'Hello World':
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'service':
+  command  => 'service nginx start',
+  provider => 'shell',
+  user     => 'root',
+  path     => '/usr/sbin/service',
 }
